51e4563690910c4a7cda4a30559c1d5ead6b3fba
Inquisitor <inquisitor@kwinta.net>
2011-06-19 03:44:33 +0300

    + Fix Nibelung proc

--- sql/mangos_spell_patch.sql
+++ sql/mangos_spell_patch.sql
@@ -1,11 +1,12 @@
 -- by Inquisitor
-DELETE FROM `spell_patch` WHERE `entry` IN (64844, 47750, 52983, 52984, 52985) AND `dbc` = 'Spell.dbc';
+DELETE FROM `spell_patch` WHERE `entry` IN (64844, 47750, 52983, 52984, 52985, 71846) AND `dbc` = 'Spell.dbc';
 INSERT INTO `spell_patch`(`entry`,`dbc`,`data`,`comment`) VALUES 
     ( '64844','Spell.dbc','213 1','Priest - Divine Hymn - Fix dmgClass = DAMAGE_CLASS_MAGIC to benefit from SPD')
    ,( '47750','Spell.dbc','86 21','Priest - Penance (Rank 1) - Target correction')
    ,( '52983','Spell.dbc','86 21','Priest - Penance (Rank 2) - Target correction')
    ,( '52984','Spell.dbc','86 21','Priest - Penance (Rank 3) - Target correction')
    ,( '52985','Spell.dbc','86 21','Priest - Penance (Rank 4) - Target correction')
+   ,( '71846','Spell.dbc','122 0','Item - Icecrown 25 Heroic Caster Weapon Proc - Fix classmask'); 
 ;
 
 -- by Antimozg
