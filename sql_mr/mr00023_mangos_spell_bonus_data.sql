a65494a7986c2648ddfc2debf054aa2ef85a4b98
Inquisitor <ssd_@ukr.net>
2011-06-21 11:16:35 +0300

    + Fix additional damage of Storm Gauntlets item.

--- sql_mr/mr00023_mangos_spell_bonus_data.sql
+++ sql_mr/mr00023_mangos_spell_bonus_data.sql
@@ -24,3 +24,9 @@ VALUES
 -- hack from Selector (it's correct, but bonus value is simple)
 DELETE FROM `spell_bonus_data` WHERE entry = 58621;
 INSERT INTO `spell_bonus_data` VALUES (58621, 0, 0, 0.08, 0, 'Glyph of Chains of Ice\r\n');
+
+-- by Inquisitor
+INSERT INTO `spell_bonus_data`(`entry`,`direct_bonus`,`dot_bonus`,`ap_bonus`,`ap_dot_bonus`,`comments`) VALUES
+    -- Item 12632 (Storm Gauntlets), proc aura 16615 (Add Lightning Dam - Weap 03), procced spell 16614 (Lightning Strike)
+    ( '16614','0','0','0','0','Item - Storm Gauntlets - Lightning Strike')
+;
