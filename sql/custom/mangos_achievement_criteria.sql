ecc7070b1397bdf7dfd8c003a011a94582d49508
Inquisitor <ssd_@ukr.net>
2011-06-22 14:10:00 +0300

    + Apply some style to our custom SQL files.

--- sql/custom/mangos_achievement_criteria.sql
+++ sql/custom/mangos_achievement_criteria.sql
@@ -1,8 +1,11 @@
-DELETE FROM achievement_criteria_requirement WHERE criteria_id IN (5601, 5603, 7408, 7412, 7415, 7418);
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN (
+    5601, 5603, 7408, 7412, 7415, 7418
+);
 INSERT INTO achievement_criteria_requirement VALUES 
-(7408, 21, 45, 0),
-(7412, 21, 42, 0),
-(7415, 21, 43, 0),
-(7418, 21, 44, 0),
-(5601, 5, 23333, 0),
-(5603, 5, 23335, 0);
+    (7408, 21, 45, 0)
+   ,(7412, 21, 42, 0)
+   ,(7415, 21, 43, 0)
+   ,(7418, 21, 44, 0)
+   ,(5601, 5, 23333, 0)
+   ,(5603, 5, 23335, 0)
+;
--- sql/custom/mangos_spell_patch.sql
+++ sql/custom/mangos_spell_patch.sql
@@ -1,5 +1,7 @@
 -- by Inquisitor
-DELETE FROM `spell_patch` WHERE `entry` IN (64844, 47750, 52983, 52984, 52985, 71846) AND `dbc` = 'Spell.dbc';
+DELETE FROM `spell_patch` WHERE `dbc` = 'Spell.dbc' AND `entry` IN (
+    64844, 47750, 52983, 52984, 52985, 71846
+);
 INSERT INTO `spell_patch`(`entry`,`dbc`,`data`,`comment`) VALUES 
     ( '64844','Spell.dbc','213 1','Priest - Divine Hymn - Fix dmgClass = DAMAGE_CLASS_MAGIC to benefit from SPD')
    ,( '47750','Spell.dbc','86 21','Priest - Penance (Rank 1) - Target correction')
@@ -10,7 +12,9 @@ INSERT INTO `spell_patch`(`entry`,`dbc`,`data`,`comment`) VALUES
 ;
 
 -- by Antimozg
-DELETE FROM `spell_patch` WHERE `dbc` = 'Achievement_Criteria.dbc' AND `entry` IN (8758);
+DELETE FROM `spell_patch` WHERE `dbc` = 'Achievement_Criteria.dbc' AND `entry` IN (
+    8758
+);
 INSERT INTO `spell_patch`(`entry`,`dbc`,`data`,`comment`) VALUES
     ( '8758','Achievement_Criteria.dbc','2 1 3 607 26 2','BG Strand of the Ancients - Storm the Beach achievement requirements correction')
 ;
