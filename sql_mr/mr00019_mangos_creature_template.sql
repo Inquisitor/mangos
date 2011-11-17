69161b3544c287b5b801551d58cf797115fa157a
Warlord123 <sergey@mail.net>
2011-11-17 15:18:57 +0600

    + Fix Searing totem Rank 1.

--- sql_mr/mr00019_mangos_creature_template.sql
+++ sql_mr/mr00019_mangos_creature_template.sql
@@ -11,3 +11,4 @@ UPDATE `creature_template` SET `spell1`='55278' WHERE `entry`='15478';
 UPDATE `creature_template` SET `spell1`='58589' WHERE `entry`='31120';
 UPDATE `creature_template` SET `spell1`='58590' WHERE `entry`='31121';
 UPDATE `creature_template` SET `spell1`='58591' WHERE `entry`='31122';
+UPDATE `creature_template` SET `spell1`='3606'  WHERE `entry`='2523'; -- Searing totem
