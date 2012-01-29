0bf3d321b2465df6229dbe80e1bfee58b478cd78
Inquisitor <inquisitor@kwinta.net>
2012-01-29 12:58:22 +0200

    Revert "[mr1631] Restore casting Blink while rooted, prevent casting Shadowstep and Killing Spree while rooted and don't remove whole holder when removing auras due to immunity (related to #689)."
    This reverts commit 35b8651d9f2b41f38b7a0087461acf8b39fa54dd.
    
    Conflicts:
    
    	src/shared/revision_R2.h

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12330,27 +12330,10 @@ void Unit::RemoveAurasAtMechanicImmunity(uint32 mechMask, uint32 exceptSpellId, 
             ++iter;
         else if (iter->second->HasMechanicMask(mechMask))
         {
-            bool removedSingleAura = false;
-
-            for(int32 i = 0; i < MAX_EFFECT_INDEX; i++)
-            {
-                if (iter->second)
-                {
-                    if ((1 << (spell->EffectMechanic[SpellEffectIndex(i)] - 1)) & mechMask)
-                    {
-                        RemoveSingleAuraFromSpellAuraHolder(iter->second, SpellEffectIndex(i));
-                        removedSingleAura = true;
-                    }
-                }
-            }
-
-            if (!removedSingleAura)
-                RemoveAurasDueToSpell(spell->Id);
+            RemoveAurasDueToSpell(spell->Id);
 
             if (auras.empty())
                 break;
-            else if (iter->second)
-                ++iter;
             else
                 iter = auras.begin();
          }
