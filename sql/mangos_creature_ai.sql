904a9aea5291ca0c458e6e3d390e37d893aab8b8
Inquisitor <inquisitor@kwinta.net>
2012-02-13 01:10:02 +0200

    Revert "creature_ai_scripts"
    This reverts commit bcfe32fe604ab5164bd01ff154b111abe67a17b7.

--- sql/mangos_creature_ai.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE creature_ai_scripts
-    ADD COLUMN event_requirement_type mediumint(10) NOT NULL DEFAULT 0 AFTER event_chance;
-
-ALTER TABLE creature_ai_scripts
-    ADD COLUMN event_requirement_value mediumint(10) NOT NULL DEFAULT 0 AFTER event_requirement_type;
