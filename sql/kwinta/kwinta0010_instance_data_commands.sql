54b28fdc1b2124ce4af8b3a2753de67e1b666b4a
Inquisitor <inquisitor@kwinta.net>
2012-03-08 12:52:20 +0200

    + Oh come on :(

--- sql/kwinta/kwinta0010_instance_data_commands.sql
+++ sql/kwinta/kwinta0010_instance_data_commands.sql
@@ -2,4 +2,4 @@ DELETE FROM `command` WHERE `name` IN ('instance getdata', 'instance setdata');
 INSERT INTO `command`(`name`, `security`, `help`)
 VALUES ('instance getdata', '3', 'Syntax: .instance getdata #data_type_id\r\n\r\nDisplay instance save data with ID specified.');
 INSERT INTO `command`(`name`, `security`, `help`)
-VALUES ('instance setdata', '3', 'Syntax: .instance setdata #data_type_id #value\r\n\r\Set value of instance save data with ID specified.');
+VALUES ('instance setdata', '3', 'Syntax: .instance setdata #data_type_id #value\r\n\r\nSet value of instance save data with ID specified.');
