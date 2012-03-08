da4b0f63ce9cae040117d7820e52e86d6913a529
Inquisitor <inquisitor@kwinta.net>
2012-03-08 12:50:02 +0200

    + Correct typo in SQL

--- sql/kwinta/kwinta0010_instance_data_commands.sql
+++ sql/kwinta/kwinta0010_instance_data_commands.sql
@@ -1,4 +1,5 @@
+DELETE FROM `command` WHERE `name` IN ('instance getdata', 'instance setdata');
 INSERT INTO `command`(`name`, `security`, `help`)
-VALUES ('istance getdata', '3', 'Syntax: .istance getdata #data_type_id\r\n\r\nDisplay instance save data with ID specified.');
+VALUES ('instance getdata', '3', 'Syntax: .instance getdata #data_type_id\r\n\r\nDisplay instance save data with ID specified.');
 INSERT INTO `command`(`name`, `security`, `help`)
-VALUES ('istance setdata', '3', 'Syntax: .istance getdata #data_type_id #value\r\n\r\Set value of instance save data with ID specified.');
+VALUES ('instance setdata', '3', 'Syntax: .instance setdata #data_type_id #value\r\n\r\Set value of instance save data with ID specified.');
