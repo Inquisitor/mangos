95b6f85a253162cb0ea92af2726bf75294e19dce
Inquisitor <inquisitor@kwinta.net>
2012-03-10 04:16:26 +0200

    [mr1704] implement and use IsNonPositiveSpell wrapper for detect some immunity exclude operations. correct some problems from mr1687.
    Conflicts:
    
    	src/game/Unit.cpp

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3482,35 +3482,18 @@ SpellMissInfo Unit::SpellHitResult(Unit *pVictim, SpellEntry const *spell, bool 
     if (pVictim->GetTypeId()==TYPEID_UNIT && ((Creature*)pVictim)->IsInEvadeMode())
         return SPELL_MISS_EVADE;
 
-    if (!(spell->Attributes & SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY))
+    if (IsPositiveSpell(spell) && IsFriendlyTo(pVictim))
+        return SPELL_MISS_NONE;
+    else if (!(spell->Attributes & SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY))
     {
-        // Check for immune
-        if (pVictim->IsImmuneToSpell(spell))
-        {
-            //Shattering Throw
-            if(spell->Id == 64382)
-            {
-                // remove immunity effects
-                pVictim->RemoveAurasDueToSpell(642); // Divine Shield
-                pVictim->RemoveAurasDueToSpell(1022); // Hand of Protection rank 1
-                pVictim->RemoveAurasDueToSpell(5599); // Hand of Protection rank 2
-                pVictim->RemoveAurasDueToSpell(10278); // Hand of Protection rank 3
-                pVictim->RemoveAurasDueToSpell(45438); // Ice Block
-            }
-            return SPELL_MISS_IMMUNE;
-        }
-
-        // All positive spells can`t miss
-        // TODO: client not show miss log for this spells - so need find info for this in dbc and use it!
-        if (IsPositiveSpell(spell->Id) && IsFriendlyTo(pVictim))
-            return SPELL_MISS_NONE;
-
         // Check for immune
         if (IsSpellCauseDamage(spell) && pVictim->IsImmunedToDamage(GetSpellSchoolMask(spell)))
             return SPELL_MISS_IMMUNE;
+        }
+
+        if (pVictim->IsImmuneToSpell(spell))
+            return SPELL_MISS_IMMUNE;
     }
-    else if (IsPositiveSpell(spell->Id) && IsFriendlyTo(pVictim))
-        return SPELL_MISS_NONE;
 
     // Try victim reflect spell
     if (CanReflect)
@@ -8403,6 +8386,7 @@ bool Unit::IsImmuneToSpell(SpellEntry const* spellInfo) const
         return true;
 
     if (!(spellInfo->AttributesEx & SPELL_ATTR_UNAFFECTED_BY_INVULNERABILITY) &&
+        IsNonPositiveSpell(spellInfo) &&
         IsImmunedToSchool(GetSpellSchoolMask(spellInfo)))
         return true;
 
