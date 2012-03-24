ac66d8a6aef2d9120cb3f143f09c94861dabeac6
Inquisitor <inquisitor@kwinta.net>
2012-03-24 03:25:00 +0200

    + Hackfixes for Shadowmourne quest chain

---
+++ sql/kwinta/kwinta0013_mangos_TEMP_shadowmourne_quest_kill_credit.sql
@@ -0,0 +1,7 @@
+-- Q: Unholy Infusion - Professor Putricide kill credit
+-- was 38587
+UPDATE `quest_template` SET `ReqCreatureOrGOId1` = 36678 WHERE `entry` = 24749;
+
+-- Q: Blood Infusion - Queen Lana'thel kill credit
+-- was 2489s0 (wtf? o_O)
+UPDATE `quest_template` SET `ReqCreatureOrGOId1` = 37955 WHERE `entry` = 24756;
